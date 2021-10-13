82f7b478f09474182edfeb383c8addc94b0e89f1
rafaelxb <rfxbarbosa@gmail.com>
2021-10-13 11:55:23 -0300

    Adição dos desafios: Animal, Coxinha do Bueno e Area do Circulo

---
+++ DesafiosCodigoCSharpDio/Animal.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafiosCodigoCSharpDio
+{
+    class Animal
+    {
+        /*Neste problema, você deverá ler 3 palavras que definem o tipo
+         *de animal possível segundo o esquema abaixo, da esquerda para a 
+         *direita.  Em seguida conclua qual dos animais seguintes foi escolhido, 
+         *através das três palavras fornecidas.
+
+         Entrada
+         A entrada contém 3 palavras, uma em cada linha, necessárias para 
+         identificar o animal segundo a figura acima, com todas as letras minúsculas.
+
+         Saída
+         Imprima o nome do animal correspondente à entrada fornecida.*/
+
+        public static void Main(string[] args)
+        {
+            string ossos;
+            string tipo;
+            string alimentacao;
+
+            ossos = Console.ReadLine();
+            tipo = Console.ReadLine();
+            alimentacao = Console.ReadLine();
+
+            if (ossos == "vertebrado")
+            {
+                if (tipo == "ave")
+                {
+                    if (alimentacao == "carnivoro")
+                    {
+                        Console.WriteLine("aguia\n");
+                    }
+                    else if (alimentacao == "onivoro")
+                    {
+                        Console.WriteLine("pomba\n");
+                    }
+                }
+                else if (tipo == "mamifero")
+                {
+                    if (alimentacao == "onivoro")
+                    {
+                        Console.WriteLine("homem\n");
+                    }
+                    else if (alimentacao == "herbivoro")
+                    {
+                        Console.WriteLine("vaca\n");
+                    }
+                }
+            }
+            else if (ossos == "invertebrado")
+            {
+                if (tipo == "inseto")
+                {
+                    if (alimentacao == "hematofago")
+                    {
+                        Console.WriteLine("pulga\n");
+                    }
+                    else if (alimentacao == "herbivoro")
+                    {
+                        Console.WriteLine("lagarta\n");
+                    }
+                }
+                else if (tipo == "anelideo")
+                {
+                    if (alimentacao == "hematofago")
+                    {
+                        Console.WriteLine("sanguessuga\n");
+                    }
+                    else if (alimentacao == "onivoro")
+                    {
+                        Console.WriteLine("minhoca\n");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DesafiosCodigoCSharpDio/Área do Círculo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafiosCodigoCSharpDio
+{
+    class Área_do_Círculo
+    {
+        /*A fórmula para calcular a área de uma circunferência é: area = π . raio2. 
+         * Considerando para este problema que π = 3.14159:
+
+        - Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
+
+        Entrada
+        A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
+
+        Saída
+        Apresentar a mensagem "A=" seguido pelo valor da variável area, 
+        conforme exemplo abaixo, com 4 casas após o ponto decimal. Utilize 
+        variáveis de dupla precisão (double). Como todos os problemas, 
+        não esqueça de imprimir o fim de linha após o resultado, caso 
+        contrário, você receberá "Presentation Error".*/
+
+        static void Main(string[] args)
+        {
+            double raio = Double.Parse(Console.ReadLine());
+            double area = 3.14159 * (raio * raio);
+
+            Console.Write("A={0}", String.Format("{0:0.0000}", area));
+        }
+
+    }
+}
