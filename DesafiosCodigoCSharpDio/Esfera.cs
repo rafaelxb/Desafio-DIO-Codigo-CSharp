fef6117c8bba4185d10507b0f4c22a66923a5667
rafaelxb <rfxbarbosa@gmail.com>
2022-02-08 15:25:25 -0300

    Adição Desafios: Esfera, Ho Ho Ho e Xenlonguinho

---
+++ DesafiosCodigoCSharpDio/Esfera.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafiosCodigoCSharpDio
+{
+    class Esfera
+    {
+        /*Faça um programa que calcule e mostre o volume de uma esfera sendo fornecido o valor
+         * de seu raio (R). A fórmula para calcular o volume é: (4/3) * pi * R3. Considere (atribua)
+         * para pi o valor 3.14159.
+
+         Dica: Ao utilizar a fórmula, procure usar (4/3.0) ou (4.0/3), pois algumas linguagens
+        (dentre elas o C++), assumem que o resultado da divisão entre dois inteiros é outro inteiro.
+
+        Entrada
+        O arquivo de entrada contém um valor de ponto flutuante (dupla precisão), 
+        correspondente ao raio da esfera.
+
+        Saída
+        A saída deverá ser uma mensagem "VOLUME" conforme o exemplo fornecido abaixo, 
+        com um espaço antes e um espaço depois da igualdade. O valor deverá ser apresentado
+        com 3 casas após o ponto.*/
+
+        static void Main(string[] args)
+        {
+
+            double pi, raio, volume;
+
+            pi = 3.14159;
+            raio = Double.Parse(Console.ReadLine());
+
+            volume = (4 * pi * Math.Pow(raio, 3)) / 3;
+
+            Console.Write("VOLUME = " + volume.ToString("F3"));
+
+        }
+    }
+}
---
+++ DesafiosCodigoCSharpDio/Xenlonguinho.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafiosCodigoCSharpDio
+{
+    class Xenlonguinho
+    {
+        /*Kogu está buscando as esferas do dragão para invocar Xenlonguinho e pedir para
+         *ele reviver seu amigo Kuriri, que infelizmente morreu na última batalha dos guerreiros Zê.
+
+        Porém Kogu está tendo muita dificuldade para encontrar as esferas, por isso 
+        Xenlonguinho que é seu conhecido há muito tempo, decidiu abrir uma exceção e 
+        aceitou ser invocado caso Kogu encontre todas as esferas cujo o número de 
+        divisores da quantidade de estrelas da esfera sejam par.
+
+        Por exemplo se existem sete esferas, Kogu não precisaria encontrar as esferas
+        de uma e quatro estrelas, pois elas tem uma quantidade ímpar de divisores, 
+        então ele só precisa pegar 5 esferas para invocar Xenlonguinho.
+
+
+
+        Como Kogu não é muito bom em contas, ele pediu para você escrever um 
+        programa que dado o total de esferas existentes, mostre a quantidade 
+        mínima de esferas que ele precisa procurar.
+
+        Entrada
+        A primeira linha consiste de um inteiro C que representa a quantidade 
+        de casos de teste. As linhas subsequentes contém um inteiro N (2 ≤ N ≤ 1000)
+        que representa a quantidade de esferas necessárias para invocar Xenlonguinho.
+
+        Saída
+        Seu programa deve exibir a quantidade mínima de esferas que Kogu tem que procurar.*/
+
+        static void Main(string[] args)
+        {
+            int numero = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numero; i++)
+            {
+                int esferas = int.Parse(Console.ReadLine());
+
+                if (esferas >= 2 || esferas <= 1000)
+                {
+                    Console.WriteLine(esferas - (Math.Floor(Math.Sqrt(esferas))));
+                }
+            }
+        }
+    }
+}
