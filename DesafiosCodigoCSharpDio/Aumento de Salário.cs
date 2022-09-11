9cfd46b2ffa0eed6ffb00ef0b2094e63aa34753c
rafaelxb <rfxbarbosa@gmail.com>
2022-09-11 19:13:29 -0300

    Novo Desafio

---
+++ DesafiosCodigoCSharpDio/Aumento de Salário.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafiosCodigoCSharpDio
+{
+    internal class Aumento_de_Salário
+    {
+        /*A empresa ABC resolveu conceder um aumento de salários a seus
+         *funcionários de acordo com a tabela abaixo:
+
+ 
+        Salário	Percentual de Reajuste
+        0 - 400.00
+        400.01 - 800.00
+        800.01 - 1200.00
+        1200.01 - 2000.00
+        Acima de 2000.00
+
+        15%
+        12%
+        10%
+        7%
+        4%
+
+        Leia o salário do funcionário e calcule e mostre o novo salário, 
+        bem como o valor de reajuste ganho e o índice reajustado, em percentual.
+
+        Entrada
+        A entrada contém apenas um valor de ponto flutuante, que pode ser maior 
+        ou igual a zero, com duas casas decimais.
+
+        Saída
+        Imprima 3 linhas na saída: o novo salário, o valor ganho de reajuste 
+        e o percentual de reajuste ganho, conforme exemplo abaixo.
+
+ 
+        Exemplo de Entrada	Exemplo de Saída
+        400.00
+
+        Novo salario: 460.00
+        Reajuste ganho: 60.00
+        Em percentual: 15 %
+
+        800.01
+
+        Novo salario: 880.01
+        Reajuste ganho: 80.00
+        Em percentual: 10 %
+        2000.00
+
+        Novo salario: 2140.00
+        Reajuste ganho: 140.00
+        Em percentual: 7 %*/
+
+        static void Main(string[] args)
+        {
+
+            double salario = 0.00;
+            double reajuste = 0.00;
+            double novoSalario = 0.00;
+            double percentual = 0.00;
+
+            salario = Convert.ToDouble(Console.ReadLine());
+
+
+            if (salario < 0.0)
+            {
+                return;
+
+            }
+            else if (salario <= 400)
+            {
+                percentual = 0.15;
+
+            }
+            else if (salario <= 800)
+            {
+                percentual = 0.12;
+
+            }
+            else if (salario <= 1200)
+            {
+                percentual = 0.10;
+
+            }
+            else if (salario <= 2000)
+            {
+                percentual = 0.07;
+
+            }
+            else
+            {
+                percentual = 0.04;
+            }
+
+            reajuste = salario * percentual;
+            novoSalario = salario + reajuste;
+
+
+            Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
+            Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
+            Console.WriteLine("Em percentual: {0} %", percentual * 100);
+
+        }
+
+
+    }
+}
---
+++ DesafiosCodigoCSharpDio/Elemento Majoritário.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafiosCodigoCSharpDio
+{
+    internal class Elemento_Majoritário
+    {
+        /*Desafio
+        Dado um array nums de tamanho n, retorne o elemento majoritário, 
+        isto é, o elemento que aparece o maior número de vezes no seu array.
+
+        Entrada
+        A primeira linha da entrada deverá ser o número referente ao tamanho
+        do seu array. As demais linhas nums serão os valores da sua array.
+
+        Saída
+        A saída deverá retornar o número que aparece o maior número de vezes
+        na sua array, ou seja, o elemento majoritário.*/
+        public static void Main(String[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] num = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(MajorityElement(num, n));
+        }
+        public static int MajorityElement(int[] nums, int n)
+        {
+            int major = nums[0];
+            int count = 1;
+            for (int i = 1; i < n; i++)
+            {
+                if (major != nums[i] && count == 1)
+                {
+                    major = nums[i];
+                    count = 1;
+                }
+                else
+                {
+                    if (major == nums[i])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        count--;
+                    }
+                }
+            }
+            return major;
+        }
+    }
+}
